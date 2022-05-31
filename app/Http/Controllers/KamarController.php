58ed08e1c7d8093572f229b30f7e155f395e33b3
salwazpw <salwapw04@gmail.com>
2022-05-31 10:19:34 +0700

    Pembuatan & pengisian Tabel kamar -msfc

---
+++ app/Http/Controllers/KamarController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kamar;
+use Illuminate\Http\Request;
+
+class KamarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $pagination = 5;
+        $pegawai = Kamar::when($request->keyword, function($query) use ($request){
+            $query
+            ->where('id','like',"%{$request->keyword}%")
+            ->orWhere('tipe_kamar','like',"%{$request->keyword}%")
+            ->orWhere('foto_kamar','like',"%{$request->keyword}%")
+            ->orWhere('harga','like',"%{$request->keyword}%");
+        })->orderBy('id')->paginate($pagination);
+
+
+            $pegawai->appends($request->only('keyword'));
+            return view('kamar.kamarindex',compact('kamar'))
+                ->with('i',(request()->input('page',1)-1)*$pagination);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Models/Kamar.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kamar extends Model
+{
+    use HasFactory;
+    protected $table = 'kamars';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'tipe_kamar',
+        'foto_kamar',
+        'harga',
+    ];
+}
