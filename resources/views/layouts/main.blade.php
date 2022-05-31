5a840a5435b303b0a93d1b6681d075be01298f5b
salwazpw <salwapw04@gmail.com>
2022-05-31 13:20:30 +0700

    Composer Sweet Alert

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,7 +22,8 @@
   <div class="preloader flex-column justify-content-center align-items-center">
     <img class="animation__wobble" src="{{asset('template/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
   </div>
-
+  
+  @include('sweetalert::alert')
   <!-- Navbar -->
   @include('layouts.navbar')
   <!-- /.navbar -->
