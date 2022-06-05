87471b6a02cc1ab25b7f7d51dda629a11a00fc9c
salwazpw <salwapw04@gmail.com>
2022-06-05 19:42:35 +0700

    modify web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.dahsboard');
+    return view('layouts.dashboard');
 });
 Route::get('/dashboard', function () {
     return view('layouts.dashboard');
