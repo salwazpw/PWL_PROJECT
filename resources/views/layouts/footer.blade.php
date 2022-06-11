7f6741679c0188c505f24af1fde3e49d5225794e
salwazpw <salwapw04@gmail.com>
2022-06-11 13:01:03 +0700

    Modify Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer text-center text-muted">
-  All Rights Reserved by Adminmart. Designed and Developed by <a
+  All Rights Reserved by Queen Z Hotel. Designed and Developed by <a
       href="https://wrappixel.com">WrapPixel</a>.
 </footer>
