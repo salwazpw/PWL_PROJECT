884dceecda447f3d58a23af9403a5d7cbe8c8e92
salwazpw <salwapw04@gmail.com>
2022-06-11 13:02:51 +0700

    Modify Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer text-center text-muted">
-  All Rights Reserved by Queen Z Hotel. Designed and Developed by <a
+  Copyright by Queen Z Hotel. Designed and Developed by <a
       href="https://wrappixel.com">WrapPixel</a>.
 </footer>
